d270aafbc4cb1e67a13d671b2a1541650199d979
akasataikisiti <nama0you0chu@gmail.com>
2023-06-15 19:20:33 +0900

    some tests

--- src/test/Util.test.ts
+++ src/test/Util.test.ts
@@ -1,4 +1,4 @@
-import { toUpperCase } from "../app/Util";
+import { getStringInfo, toUpperCase } from "../app/Util";
 
 describe("toUpperCase", () => {
   it("should return uppercase of valid string", () => {
@@ -11,5 +11,36 @@ describe("toUpperCase", () => {
 
     // assert
     expect(actual).toBe(expected);
+  });
+  it.only("should return info for valid string", () => {
+    const actual = getStringInfo("My-String");
+
+    expect(actual.lowerCase).toBe("my-string");
+    // expect(sut.extraInfo).toBe({});
+    expect(actual.extraInfo).toStrictEqual({});
+
+    expect(actual.characters.length).toBe(9);
+    expect(actual.characters).toHaveLength(9);
+
+    expect(actual.characters).toEqual([
+      "M",
+      "y",
+      "-",
+      "S",
+      "t",
+      "r",
+      "i",
+      "n",
+      "g",
+    ]);
+    expect(actual.characters).toContain<string>("M");
+    expect(actual.characters).toEqual(
+      expect.arrayContaining(["t", "r", "i", "n", "g", "M", "y", "-", "S"])
+    );
+
+    expect(actual.extraInfo).not.toBe(undefined);
+    expect(actual.extraInfo).not.toBeUndefined;
+    expect(actual.extraInfo).toBeDefined();
+    expect(actual.extraInfo).toBeTruthy();
   });
 });
